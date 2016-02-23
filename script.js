8bd2622c98adb5b00ec662dcd5df5ce67eb074d4
Patrick Moran <patrick_moran@mac.com>
2016-02-23 16:40:03 -0500

    War functionality on ties, some styling updates

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var game = {
   hands: [],
   players: ["Player 1", "Computer"],
   stage: [],
+  war: [],
 
   newDeck: function() {
     var self = this;
@@ -48,18 +49,32 @@ var game = {
     }
   },
 
-  setUpStage: function() {
-    // put the top card in each players hand to the staging area and compare
+  cardsToStage: function() {
+    // put the top card in each players hand to the staging area
     for (var i = 0; i < this.players.length; i++) {
       this.stage.push({player: this.players[i], card: this.hands[i].cards.pop()});
     }
   },
 
   getWinner: function() {
+    //compare cards in stage by weight
     var self = this;
     this.stage.sort(function(a, b) {
       return b.card.weight - a.card.weight;
     });
+
+  },
+
+  reset: function() {
+    this.deck = [];
+    this.hands = [];
+    this.stage = [];
+    this.war = [];
+    this.newDeck();
+    this.shuffleDeck();
+    this.setUpHands();
+    this.dealCards();
+    updateRemaining();
   }
 };
 
@@ -67,33 +82,40 @@ game.newDeck();
 game.shuffleDeck();
 game.setUpHands();
 game.dealCards();
-// game.setUpStage();
 
-// function createStage() {
-//   // target each hand div then append child (stage) to that div
-//   var handDivs = document.querySelectorAll(".hand");
-//   for (var i = 0; i < handDivs.length; i++) {
-//     var stagedCard = document.createElement("div").setAttribute("class", "stage");
-//     console.log(handDivs[i]);
-//     handDivs[i].appendChild(stagedCard);
-//   }
-// }
-
+var resetButton = document.getElementById("reset");
 var flipButton = document.getElementById("flip");
 var stagingArea = document.querySelector(".stage-area");
-flipButton.addEventListener("click", function() {
-  if (game.hands[0].cards.length > 0 && game.hands[1].cards.length > 0) {
-  flipCards();
-  game.stage = [];
-} else {
-  alert("Game Over!");
-}
+
+resetButton.addEventListener("click", function() {
+  game.reset();
 });
 
+flipButton.addEventListener("click", function() {
+  if (game.hands[0].cards.length > 0 && game.hands[1].cards.length > 0) {
+    flipCards();
+    game.stage = [];
+  } else {
+    alert("Game Over!");
+  }
+  updateRemaining();
+});
 
 function flipCards() {
-  game.setUpStage();
-  setStage();
+  game.cardsToStage();
+  showCards();
+  if (buildWarStage()) {
+    warWinner();
+    for (var x = 0; x < game.war.length; x++){
+      if (game.hands[x].name === game.war[0].player) {
+        for (var y = 0; y < 4; y++) {
+          game.hands[x].cards.unshift(game.war[0].cards[y]);
+          game.hands[x].cards.unshift(game.war[1].cards[y]);
+        }
+      }
+    }
+    game.war = [];
+  }
   game.getWinner();
   var winner = game.stage[0].player;
   for (var i = 0; i < game.stage.length; i++) {
@@ -103,28 +125,61 @@ function flipCards() {
       }
     }
   }
-  console.log(game.hands[0].cards.length);
-  // game.stage = [];
-  // alert(message);
 }
 
-function setStage() {
+function showCards() {
   for (var i = 0; i < 2; i++) {
     var stageCard = document.createElement("div");
     stageCard.setAttribute("class","stage-card");
     stagingArea.appendChild(stageCard);
-    //this is putting every high card on players side...
-    stageCard.innerText = game.stage[i].card.value + " " + game.stage[i].card.suit;
+    stageCard.innerText = game.stage[i].card.value + "\n";
+    stageCard.innerText += game.stage[i].card.suit;
+    stageCard.style.fontSize = "48px";
     setTimeout(function () {
       var cards = document.querySelectorAll(".stage-card");
       for (var i = 0; i < cards.length; i++) {
         stagingArea.removeChild(cards[i]);
       }
     }, 2000);
-    }
+  }
 }
 
-function checkTie() {
-  // if the card weight is equal return card to players hand
+function buildWarStage() {
+  // IF the card weight is equal
+  // go back 4 indexes in hands
+  // compare again
+  // IF equal go back 4 more indexes and compare again
+  // repeat until someone wins
+  // all cards go to winners hand
+  if (game.stage[0].card.weight === game.stage[1].card.weight) {
+    game.stage.forEach(function(i) {
+      game.war.push({player: i.player, cards: []});
+    });
+    game.hands.forEach(function(i) {
+      if (i.name === game.war[0].player) {
+        for (var j = 0; j < 4; j++) {
+          game.war[0].cards.push(i.cards.pop());
+        }
+      } else if (i.name === game.war[1].player) {
+        for (var k = 0; k < 4; k++) {
+          game.war[1].cards.push(i.cards.pop());
+        }
+      }
+    });
+  }
+  return true;
+}
 
+function warWinner() {
+  game.war.sort(function(a, b) {
+    return b.cards[3].weight - a.cards[3].weight;
+  });
+}
+
+function updateRemaining() {
+  var playerRemaining = document.querySelector(".remaining .player");
+  var computerRemaining = document.querySelector(".remaining .computer");
+
+  playerRemaining.innerHTML = "Remaining Cards: " + game.hands[0].cards.length;
+  computerRemaining.innerHTML = "Remaining Cards: " + game.hands[1].cards.length;
 }
